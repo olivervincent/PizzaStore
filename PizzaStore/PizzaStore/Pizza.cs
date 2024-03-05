1ed3201785a9d5b69ee91e12668eb14400cd56ce
Oliver Vincent <oliverlund3@gmail.com>
2024-03-05 09:36:21 +0100

    Add files via upload

---
+++ PizzaStore/PizzaStore/Pizza.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace PizzaStore
+{
+	public class Pizza
+	{
+        private string _pizzaName;
+
+        public Pizza(string pizzaName)
+		{
+			_pizzaName = pizzaName;
+		}
+
+		public string GetPizzaName()
+		{
+			return _pizzaName;
+		}
+
+        public override string ToString()
+        {
+            return _pizzaName;
+        }
+    }
+}
+
